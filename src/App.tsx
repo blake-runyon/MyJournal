f13c29b83af82904988513e4ce017b4deaacdd4d
blake-runyon <johnathan.runyon@snhu.edu>
2022-07-26 20:22:52 -0400

    posts component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import { PostService } from './services/PostService';
 import Header from './globals/Header';
-import {Post} from './components/Post';
+import {Posts} from './components/Post';
 function App() {
   const postService = new PostService();
   return (
     <>
     <Header />
-    <Post id="abc"/>
+    <Posts />
     </>
   );
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
+import { PostService } from '../services/PostService';
+import moment from 'moment';
 
-export const Post = (props:{id: string}) => {
-    const { id } = props;
-    
+export const Posts = () => {
+    const pService = new PostService();
+    const [post, setPost] = React.useState<any[]>([]);
+
+    useEffect(() => {
+      pService.GetPosts().then(data => {
+        setPost(data);
+      })
+    }, [])
+
     return (
       <>
         <Container>
           <Row>
-            <Col xs="12" md="4">1</Col>
-            <Col xs="12" md="8">{id}</Col>
-          </Row>
-          <Row>
-            <Col xs="12" md={{ span: 3, offset: 4 }}>2</Col>
+            { post.map((p: any) => {
+              return (
+                <>
+                <Col xs="12" md="6">
+                  <Card className='m-1'>
+                    <Card.Body>
+                      <Card.Title>
+                        <h3 style={{color: "#70c00f"}}>{p.title}</h3>
+                      </Card.Title>
+                      <Card.Subtitle>
+                        <sup>{moment(p.createdAt).format('M/D/yyyy')}</sup>
+                      </Card.Subtitle>
+                      <Card.Text>
+                        <p>{p.content}</p>
+                        <a href={`/post/${p._id}`}>Read More</a>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                </>
+              )
+            }) }
           </Row>
         </Container>
       </>
